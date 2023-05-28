97bb1eaa7a45bb12f93837a9009bb60ed6f22eaa
malfoy.obsession <malfoy.obsession@gmail.com>
2023-05-28 00:21:59 -0600

    feat: se añadio el archivo js de la pagina de inicio de sesión

---
+++ Proyecto-cajero/js/index.js
@@ -0,0 +1,37 @@
+function datosUsuario(){
+  let emailInput = document.getElementById('emailUser');
+  let passwordInput = document.getElementById('passwordUser');
+
+  emailInput = emailInput.value;
+  passwordInput = passwordInput.value;
+
+  console.log(emailInput);
+  console.log(passwordInput);
+
+  if (emailInput == persona.email){
+      if(passwordInput == persona.password){
+          window.location.href = './main.html';
+      }
+      else{
+          console.log("Error");
+          mensajeUsuario.innerHTML = "La contraseña es incorrecta";
+      }
+  } else {
+      console.log("Error");
+      mensajeUsuario.innerHTML = "El correo es incorrecto";
+  }
+}
+
+const persona =
+  { email: 'aemond@gmail.com', password: 1234, saldo: 20000 }
+
+
+
+
+let btnLogin = document.getElementById('btnLogin');
+let mensajeUsuario = document.getElementById('msgUser');
+
+btnLogin.addEventListener('click', datosUsuario);
+
+
+
